c5114ba5ac0d3adfd41fc97828a0a2ff7b7a824d
gavinev98 <gavin.everett48@hotmail.com>
2018-09-28 17:17:36 +0100

    Added in a Message Dialog and 2 images to VideoServiceSimulator Class.

--- Design Patterns/src/VideoServiceSimulator.java
+++ Design Patterns/src/VideoServiceSimulator.java
@@ -1,7 +1,16 @@
+
+import javax.swing.*;
+import java.awt.*;
+
 public class VideoServiceSimulator {
 
     public static void main(String args[])
     {
-        
+        ImageIcon icon = new ImageIcon(VideoServiceSimulator.class.getResource("/youtubelogo.jpg"));
+
+        JOptionPane.showMessageDialog(null,"Welcome to VideoPlayer!" , "Youtube",JOptionPane.INFORMATION_MESSAGE,
+                icon);
+
+
     }
 }
